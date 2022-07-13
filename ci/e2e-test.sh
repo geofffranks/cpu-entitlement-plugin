3a84596d6f945d595e9f6fecd11373e2078c0e82
Geoff Franks <gfranks@vmware.com>
2022-07-13 20:45:10 +0000

    Install ginkgo in CI script for new image
    Signed-off-by: Renee Chu <reneec@vmware.com>

--- ci/e2e-test.sh
+++ ci/e2e-test.sh
@@ -2,5 +2,6 @@
 set -e
 
 pushd cpu-entitlement-plugin
+go install github.com/onsi/ginkgo/ginkgo@latest
 make e2e-test
 popd
--- ci/unit-tests.sh
+++ ci/unit-tests.sh
@@ -5,4 +5,5 @@ go version
 
 cd src/code.cloudfoundry.org/cpu-entitlement-plugin
 
+go install github.com/onsi/ginkgo/ginkgo@latest
 make test
