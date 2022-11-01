28fed681fdc710bb4a10c73c737e4c222c87ff8a
Amelia Downs <adowns@vmware.com>
2022-11-01 17:32:56 +0000

    ci: allow users to pass config for tests

--- ci/integration-test.sh
+++ ci/integration-test.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -n "$CONFIG" ]; then
+  CF_API=$(cat ${CONFIG_PATH} | jq .api)
+  CF_USERNAME=$(cat ${CONFIG_PATH} | jq .admin_user)
+  CF_PASSWORD=$(cat ${CONFIG_PATH} | jq .admin_password)
+fi
+
+# sample CONFIG file
+# {
+#   "admin_password": "meow",
+#   "admin_user": "admin",
+#   "api": "meow.com"
+# }
+
 pushd cpu-entitlement-plugin
 go install github.com/onsi/ginkgo/ginkgo@latest
 make integration-test
