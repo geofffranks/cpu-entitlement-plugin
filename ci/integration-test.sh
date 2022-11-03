249a63bd5e910ae77e22185782f761aa3d6d781c
Amelia Downs <adowns@vmware.com>
2022-11-03 20:38:43 +0000

    Revert "ci: add debugging to task"
    This reverts commit d73bd0091c30ae97069cad80735f8af676e8ddfb.

--- ci/integration-test.sh
+++ ci/integration-test.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir secrets
 if [ -n "$CONFIG" ]; then
   export CF_API=$(cat ${CONFIG} | jq -r .api)
   export CF_USERNAME=$(cat ${CONFIG} | jq -r .admin_user)
   export CF_PASSWORD=$(cat ${CONFIG} | jq -r .admin_password)
-  echo "from config" > secrets/secrets.txt
+  export ROUTER_CA_CERT=$(cat ${CONFIG} | jq -r .lb_cert)
 fi
-echo $CF_API >> secrets/secrets.txt
-echo $CF_USERNAME >> secrets/secrets.txt
-echo $CF_PASSWORD >> secrets/secrets.txt
 
 # sample CONFIG file
 # {
 #   "admin_password": "meow",
 #   "admin_user": "admin",
-#   "api": "meow.com"
+#   "api": "meow.com",
+#   "lb_cert": "BEGIN MEOW CERT"
 # }
 
 pushd cpu-entitlement-plugin
